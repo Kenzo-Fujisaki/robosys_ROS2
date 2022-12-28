1788b492bc0a75e467af45f58768042e366b1cd7
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-12-28 11:07:18 +0900

    Register Module

---
+++ robosys_ROS2/fortune.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022 Kenzo Fujisaki
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+import random
+
+rclpy.init()
--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'fortune = robosys_ROS2.fortune:main',
+            #'result = robosys_ROS2.result:main',
         ],
     },
 )
