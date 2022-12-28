9053f51689a16e76eb396d20bc40bf5ce19f5034
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-12-28 11:56:04 +0900

    create pub

--- robosys_ROS2/fortune.py
+++ robosys_ROS2/fortune.py
@@ -6,4 +6,30 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 import random
 
-rclpy.init()
+class Fortune():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "number", 10)
+
+    def cb(self, node):
+        input_key = input()
+        print("[N]:おみくじ,[L]:恋みくじ")
+        if input_key == 'n':
+            normal = random.randint(1,7)
+            self.pub.publish(normal)
+
+        elif input_key == 'l':
+            love = random.randint(1,7)
+            self.pub.publish(love)
+
+        else:
+            print("error")
+            rclpy.shutdown(node)
+
+def main():
+    rclpy.init()
+    node = Node("fortune")
+    fortune = Fortune()
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
