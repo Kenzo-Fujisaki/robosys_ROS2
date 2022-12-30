da1d38b521377afddef029f8ee14fe7bc1ab8cc4
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-12-30 15:19:06 +0900

    add text file

--- robosys_ROS2/fortune.py
+++ robosys_ROS2/fortune.py
@@ -6,19 +6,16 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 import random
 
-class Fortune():
-    def __init__(self,node):
-        self.pub = node.create_publisher(Int16, "number", 10)
+rclpy.init()
+node = Node("fortune")
+pub = node.create_publisher(Int16, "number", 10)
+num = random.randint(1,7)
 
-    def cb(self):
-        num = random.randint(1,7)
-        self.pub.publish(num)
+def cb():
+    global num
+    msg = Int16()
+    msg.data = num
+    pub.publish(msg)
 
-def main():
-    rclpy.init()
-    node = Node("fortune")
-    fortune = Fortune(node)
-    rclpy.spin(node)
-
-if __name__ == '__main__':
-    main()
+node.create_timer(0.5, cb)
+rclpy.spin(node)
--- robosys_ROS2/result.py
+++ robosys_ROS2/result.py
@@ -5,25 +5,40 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cb(num):
-    global node
-    node = num
-    if node == 1:
-        print("大吉")
-    elif node == 2:
-        print("吉")
-    elif node == 3:
-        print("中吉")
-    elif node == 4:
-        print("小吉")
-    elif node == 5:
-        print("末吉")
-    elif node == 6:
-        print("凶")
-    elif node == 7:
-        print("大凶")
+def cb(msg):
+    omikuji = msg.data
+
+    if omikuji == 1:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/daikichi.txt','r')
+        data = f.read()
+        print(data)
+    elif omikuji == 2:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/kichi.txt','r')
+        data = f.read()
+        print(data)
+    elif omikuji == 3:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/tyuukichi.txt','r')
+        data = f.read()
+        print(data)
+    elif omikuji == 4:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/syouichi.txt','r')
+        data = f.read()
+        print(data)
+    elif omikuji == 5:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/suekichi.txt','r')
+        data = f.read()
+        print(data)
+    elif omikuji == 6:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/kyou.txt','r')
+        data = f.read()
+        print(data)
+    elif omikuji == 7:
+        f = open('/home/kenzo-fujisaki/ros2_ws/src/robosys_ROS2/robosys_ROS2/daikyou.txt','r')
+        data = f.read()
+        print(data)
+
 
 rclpy.init()
-node = Node("listener")
+node = Node("result")
 pub = node.create_subscription(Int16, "number", cb, 10)
 rclpy.spin(node)
