5265226153c52ae075d32391eb3ea23876ef22ff
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-12-30 23:18:05 +0900

    Modify

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Kenzo Fujisaki
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+ros2 launch robosys_ROS2 omikuji.launch.py > /tmp/robosys_ROS2.log
+
+cat /tmp/robosys_ROS2.log | grep 吉
