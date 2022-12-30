6048736b804e386e58d5c0c728212432a52b5418
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-12-30 18:44:33 +0900

    Add launch

---
+++ launch/omikuji.launch.py
@@ -0,0 +1,18 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+    fortune = launch_ros.actions.Node(
+        package='robosys_ROS2',
+        executable='fortune',
+        output='screen'
+        )
+    result = launch_ros.actions.Node(
+        package='robosys_ROS2',
+        executable='result',
+        output='screen'
+        )
+
+    return launch.LaunchDescription([fortune, result])
